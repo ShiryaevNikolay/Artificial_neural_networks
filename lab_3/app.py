f764e141d034c08e6efd779e7790c14603000a59
ShiryaevNikolay <deendeeps@list.ru>
2021-03-26 21:33:37 +0300

    теперь можно рисовать оттенками серого

--- lab_3/app.py
+++ lab_3/app.py
@@ -33,14 +33,37 @@ class App(Tk):
                 ))
 
     def mouseMoveB1(self, event):
-        self.paint(math.floor(event.x / self.scale), math.floor(event.y / self.scale))
+        # self.paint(math.floor(event.x / self.scale), math.floor(event.y / self.scale))
+        self.paint(event)
 
     def mouseMoveB3(self, event):
-        self.paint(math.floor(event.x / self.scale), math.floor(event.y / self.scale), "white")
+        # self.paint(math.floor(event.x / self.scale), math.floor(event.y / self.scale), "white")
+        self.paint(event, "right")
 
-    def paint(self, x, y, color="blue"):
-        print(x, y)
-        self.c.itemconfigure(self.pixels[x][y], fill=color)
+    def paint(self, event, mouse_press="left"):
+        # self.c.itemconfigure(self.pixels[x][y], fill=color)
+        # inputs = [0 for i in range(self.w * self.h)]
+        mx = event.x / self.scale
+        my = event.y / self.scale
+        for i in range(self.w):
+            for j in range(self.h):
+                dist = (i - mx)**2 + (j - my)**2  # Высчитываем радиус от тоординаты мыши, то каждого пикселя в итерации
+                if dist < 1:  # Задаем минимайный размер радиуса =1
+                    dist = 1
+                dist *= dist
+                if mouse_press == "left":
+                    self.colors[i][j] -= 0.1 / dist
+                elif mouse_press == "right":
+                    self.colors[i][j] += 0.1 / dist
+                if self.colors[i][j] > 1:
+                    self.colors[i][j] = 1
+                if self.colors[i][j] < 0:
+                    self.colors[i][j] = 0
+                print(self.colors[i][j])
+                color = int(self.colors[i][j] * 255)
+                color = (color << 16) | (color << 8) | color
+                self.c.itemconfigure(self.pixels[i][j], fill="#{0:06X}".format(color))
+
 
     # def paint(self, event):
     #     inputs = [0 for i in range(784)]
