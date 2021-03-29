c5be97fa16f38d158e2ad57c963ce19f08aa2f33
ShiryaevNikolay <deendeeps@list.ru>
2021-03-29 14:29:38 +0300

    При нажатии на C очищается canvas

--- lab_3/app.py
+++ lab_3/app.py
@@ -16,18 +16,12 @@ class App(Tk):
         self.colors = [[0 for j in range(self.w)] for i in range(self.h)]
 
         self.create_canvas()
+        self.create_info()
 
-        fr_btn = Frame(self, highlightthickness=10)
-        fr_btn.grid(column=0, row=0)
-
-        btn_clear = button(fr_btn, "C: Очистить", _callback=None)
-        btn_clear.grid(column=0, row=0, padx=5)
-
-        btn_check = button(fr_btn, "Проверить")
-        btn_check.grid(column=1, row=0, padx=5)
+        self.bind("c", self.press_c)
 
         fr_perceptron = Frame(self)
-        fr_perceptron.grid(column=1, row=1)
+        fr_perceptron.grid(column=1, row=2)
         Label(fr_perceptron, text="Номер персептрона").grid(column=1, row=1)
 
     def create_canvas(self):
@@ -35,7 +29,26 @@ class App(Tk):
         self.createPixels()
         self.c.bind("<B1-Motion>", self.mouseMoveB1)
         self.c.bind("<B3-Motion>", self.mouseMoveB3)
-        self.c.grid(column=0, row=1)
+        self.c.grid(column=0, row=2)
+
+    def press_c(self, event):
+        self.clearCanvas()
+
+    def create_info(self):
+        fr_info = Frame(self, highlightthickness=5)
+        fr_info.grid(column=0, row=0)
+
+        ''' Добавляем информацию о том, как рисовать '''
+        lkm_text = Label(fr_info, text="Рисовать: ЛКМ")
+        lkm_text.grid(column=0, row=0, padx=10, pady=5)
+        rkm_text = Label(fr_info, text="Стирать: ПКМ")
+        rkm_text.grid(column=0, row=1, padx=10, pady=5)
+
+        ''' Добавляем кнопки '''
+        btn_clear = ButtonView(fr_info, "C: Очистить", _callback=self.clearCanvas)
+        btn_clear.grid(column=1, row=0, padx=10, pady=5)
+        btn_check = ButtonView(fr_info, "Проверить")
+        btn_check.grid(column=2, row=0, padx=10, pady=5)
 
     def createPixels(self):
         for i in range(self.w):
@@ -46,7 +59,8 @@ class App(Tk):
                 self.pixels[i].append(self.c.create_rectangle(
                     i * self.scale, j * self.scale,
                     i * self.scale + self.scale, j * self.scale + self.scale,
-                    fill="#{0:06X}".format(color)
+                    fill="#{0:06X}".format(color),
+                    width=0
                 ))
 
     def mouseMoveB1(self, event):
@@ -73,4 +87,10 @@ class App(Tk):
                 self.colors[i][j] = 0
             color = int(self.colors[i][j] * 255)
             color = (color << 16) | (color << 8) | color
+            self.c.itemconfigure(self.pixels[i][j], fill="#{0:06X}".format(color))
+
+    def clearCanvas(self):
+        for i, j in itertools.product(range(self.w), range(self.h)):
+            self.colors[i][j] = 0
+            color = (0 << 16) | (0 << 8) | 0
             self.c.itemconfigure(self.pixels[i][j], fill="#{0:06X}".format(color))
--- views/buttons.py
+++ views/buttons.py
@@ -1,22 +1,19 @@
 from tkinter import *
 
 
-def button(root, text, _callback=None):
-    return Button(
-        root,
-        text=text,
-        borderwidth=0,
-        bg="#3f51b5",
-        fg="#ffffff",
-        highlightcolor="#29b6f6",
-        activeforeground="#FFFFFF",
-        activebackground="#5c6bcd",
-        highlightthickness=5,
-        # highlightbackground="#37d3ff"
-        command=callback_btn(_callback)
-    )
+class ButtonView(Button):
 
-
-def callback_btn(_callback=None):
-    if _callback:
-        _callback()
+    def __init__(self, root, text, _callback=None):
+        super().__init__(
+            root,
+            text=text,
+            borderwidth=0,
+            bg="#3f51b5",
+            fg="#ffffff",
+            highlightcolor="#29b6f6",
+            activeforeground="#FFFFFF",
+            activebackground="#5c6bcd",
+            highlightthickness=5,
+            # highlightbackground="#37d3ff"
+            command=_callback
+        )
